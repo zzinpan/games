8b0c100cc77104984f4cf0325220466c801fa4af
zzinpan <zzinpan@kakao.com>
2023-05-16 01:07:59 +0900

    feat: 상단 헤더 샘플을 넣어보고 간단히 클릭이벤트를 구현해본다

---
+++ pages/games.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderLayout from "@/app/layouts/HeaderLayout";
+
+const GamesPage: React.FC = () => {
+    return (
+        <HeaderLayout>
+            <div>게임 페이지</div>
+        </HeaderLayout>
+    );
+};
+
+export default GamesPage;
